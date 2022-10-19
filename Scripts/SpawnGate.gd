520cf2e1c41b1e27c7f45d5b7c2e7b0cde989ae9
FastShowDev <zlFastShow@hotmail.com>
2022-10-19 20:59:32 -0300

    Adiciona script de abertura de porta

--- Scripts/SpawnGate.gd
+++ Scripts/SpawnGate.gd
@@ -6,13 +6,17 @@ var is_player_inside: bool = false
 onready var anim = $AnimetedSprite
 
 func _ready():
+	is_player_inside = false
 	self.playing = false
 	self.frame = 0
-	
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact_item") and is_player_inside and not is_open:
 		is_open = true
 		self.frame = 1
+	elif event.is_action_pressed("interact_item") and is_open and is_player_inside:
+		print("Entrando na dungeon!!!")
+		pass
 		
 func _on_Area2D_body_entered(_player: KinematicBody2D) -> void:
 	if _player is KinematicBody2D:
