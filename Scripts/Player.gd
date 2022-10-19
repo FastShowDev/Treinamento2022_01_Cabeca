9bcaad25f78580d336fc4da22f6cc678859d5d2e
FastShowDev <zlFastShow@hotmail.com>
2022-10-19 05:28:20 -0300

    Adiciona animação de movimento do jogador atualizada

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,49 +5,73 @@ var health = 100
 var keys = 0
 var interactDist = 20
 var velocity = Vector2.ZERO
-var facingDir = Vector2.ZERO
 
-onready var raycast = $RayCast2D
-onready var anim = $AnimatedSprite
+onready var animeted_sprite = $AnimatedSprite
+onready var animation = $AnimationPlayer
+onready var collision = $AttackArea/CollisionShape2D
+
+var can_attack: bool = false
+var player_direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
 
-func _physics_process(delta):
-	velocity = Vector2()
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		facingDir = Vector2(0,-1)
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-		facingDir = Vector2(0,1)
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		facingDir = Vector2(-1,0)
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		facingDir = Vector2(1,0)
-		
-	velocity = velocity.normalized()
-	move_and_slide(velocity * speed, Vector2.ZERO)
+func _physics_process(delta: float) -> void:
+	move()
+	verify_direction()
+	animate()
+	#attack()
+
+func move() -> void:
+	var direction_vector: Vector2 = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	).normalized()
 	
+	velocity = direction_vector * speed
+	velocity = move_and_slide(velocity)
+
+func verify_direction() -> void:
 	if velocity.x > 0:
-		play_animation("right")
+		player_direction = Vector2(1,0)
 	elif velocity.x < 0:
-		play_animation("left")
+		player_direction = Vector2(-1,0)
 	elif velocity.y > 0:
-		play_animation("down")
+		player_direction = Vector2(0,1)
 	elif velocity.y < 0:
-		play_animation("up")
-	elif facingDir.x == 1:
-		play_animation("idle_right")
-	elif facingDir.x == -1:
-		play_animation("idle_left")
-	elif facingDir.y == 1:
-		play_animation("idle_down")
-	elif facingDir.y == -1:
-		play_animation("idle_up")
+		player_direction = Vector2(0,-1)
+
+func attack() -> void:
+	pass
+		
+
+func animate() -> void:
+	if velocity != Vector2.ZERO:
+		if velocity.x > 0:
+			animation.play("move_right")
+		elif velocity.x < 0:
+			animation.play("move_left")
+		elif velocity.y > 0:
+			animation.play("move_down")
+		elif velocity.y < 0:
+			animation.play("move_up")
+	else:
+		animation.stop()
+		if player_direction.x == 1:
+			animeted_sprite.play("idle_right")
+		elif player_direction.x == -1:
+			animeted_sprite.play("idle_left")
+		elif player_direction.y == 1:
+			animeted_sprite.play("idle_down")
+		elif player_direction.y == -1:
+			animeted_sprite.play("idle_up")
+			
+	
 		
 func play_animation(anim_name):
-	if anim.animation != anim_name:
-		anim.play(anim_name)
+	pass
+	
+
+	
+func on_animation_finished(anim_name):
+	pass
