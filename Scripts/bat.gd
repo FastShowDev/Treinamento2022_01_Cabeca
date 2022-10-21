7e0af053876d277e913cec438620286fd42448d6
semcovici <pedrosemcovici@outlook.com>
2022-10-21 15:24:34 -0300

    Dungeon base wall && adicionando o rayCast para o eixo Y

--- Scripts/bat.gd
+++ Scripts/bat.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
+#
+#Esse inimigo tem uma tragetória simples,
+#ele apenas voa aleatóriamente e, ao colidir 
+#com o cenário, ele muda de direção
 
-
-export var speed = 100
+export var speed = 55
 export var health = 1 
 var velocity = Vector2.ZERO
 var move_direction_x = -1
@@ -19,10 +22,22 @@ func _physics_process(delta: float) -> void:
 	else:
 		$texture.flip_h = false
 		
-	if $ray_wall.is_colliding():
+		
+	
+	if $ray_wall_x.is_colliding():
 		$texture.flip_h != $texture.flip_h
-		$ray_wall.scale.x *= -1 
+		$ray_wall_x.scale.x *= -1
 		move_direction_x *= -1
 		move_direction_y *= -1
+		$ray_wall_y.scale *= -1
+
+	if $ray_wall_y.is_colliding():
+		$ray_wall_y.scale *= -1
+		move_direction_x *= 1
+		move_direction_y *= -1
+#
 		
+
+	
+	
 	
