8f17ac4cab6c0338026cfe55c334da48125e934f
FastShowDev <zlFastShow@hotmail.com>
2022-10-25 02:40:22 -0300

    Alguns ajustes no script dos objetos interagíveis e spawn

--- Scripts/S_Base/InteractObject.gd
+++ Scripts/S_Base/InteractObject.gd
@@ -1,14 +1,16 @@
 extends StaticBody2D
 
 var is_player_close: bool = false
+var is_open: bool = false
 var text: String = "Meu texto"
 
+const bar: PackedScene = preload("res://Interfaces/DialogBar.tscn")
 onready var interact_baloom = $InteractBaloom
 
 
 func _ready():
 	interact_baloom.hide()
-	
+		
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		interact_baloom.show()
