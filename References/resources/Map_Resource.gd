6c6436fefd0cf05eec14478a1a780befe2a63700
FastShowDev <zlFastShow@hotmail.com>
2022-10-31 03:05:50 -0300

    Cria resource para salvar dados do mapa

---
+++ References/resources/Map_Resource.gd
@@ -0,0 +1,16 @@
+class_name Map
+extends Resource
+
+#Node information: 
+export var map_name = ""
+export var scene_path = ""
+
+#Map structures:
+#["Structure_name", "x.position", "y.position", open_state = false/true]
+export var structures: Array = []
+
+#Enemies information: 
+export var enemies: Array = []
+
+#Itesn information:
+export var itens: Array = []
