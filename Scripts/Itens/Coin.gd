f7b45d516f57fe142b910b9582d8fe3dd4c212ab
FastShowDev <zlFastShow@hotmail.com>
2022-11-05 11:23:39 -0300

    Adiciona som  de coleta de moeda

--- Scripts/Itens/Coin.gd
+++ Scripts/Itens/Coin.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite
 
 onready var anim = $CoinAnimationPlayer
+onready var sound = $CollectFX
 
 func _ready():
 	pass
@@ -11,4 +12,7 @@ func _physics_process(delta):
 func _on_Area2D_body_entered(body: KinematicBody2D):
 	if body.name == "Player":
 		body.collect_coin()
+		sound.playing = true
+		self.visible = false
+		yield(sound, "finished")
 		queue_free()
