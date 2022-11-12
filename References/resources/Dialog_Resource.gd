5dba4e0f0a96a0f9b1067d7af64a94675215367c
FastShowDev <zlFastShow@hotmail.com>
2022-11-12 01:38:16 -0300

    Remove métodos inúteis do Dialog-Resource

--- References/resources/Dialog_Resource.gd
+++ References/resources/Dialog_Resource.gd
@@ -3,10 +3,3 @@ extends Resource
 
 var is_dialog_open: bool = false
 var dialog_text: String = ""
-
-func set_dialog_text(text:String) -> void:
-	dialog_text =  text
-	emit_changed()
-
-func get_dialog_text() -> String:
-	return dialog_text
