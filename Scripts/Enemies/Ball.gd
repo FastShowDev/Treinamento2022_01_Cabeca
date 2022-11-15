e98859a4d8ff5cbc9c6e88046ab69581f0e68c3d
FastShowDev <zlFastShow@hotmail.com>
2022-11-15 04:21:25 -0300

    Arruma Script da Ball que deixava de movimentar-se quando a cena erra carregada e a Ball estivesse colidindo apenas em baixo + arruma Layers

--- Scripts/Enemies/Ball.gd
+++ Scripts/Enemies/Ball.gd
@@ -2,16 +2,60 @@ extends KinematicBody2D
 
 const path: String = "res://Models/Enemies/Ball.tscn"
 
-func _ready():
-	pass # Replace with function body.
+onready var ray_up: RayCast2D = $rayCast/up
+onready var ray_down: RayCast2D = $rayCast/down
+onready var ray_right: RayCast2D = $rayCast/right
+onready var ray_left: RayCast2D = $rayCast/left
+
+var speed: int = 250
+var direction_vector: Vector2
+var velocity: Vector2
 
 
-func _on_DetectionArea_body_entered(body):
-	if body.name == "Player":
-		pass
+func _ready() -> void:
+	if ray_up.is_colliding():
+		if ray_right.is_colliding():
+			direction_vector = Vector2(0, 1)
+		else:
+			direction_vector = Vector2(1, 0)
+	elif ray_down.is_colliding():
+		if ray_right.is_colliding():
+			direction_vector = Vector2(-1, 0)
+		else:
+			direction_vector = Vector2(0, -1)
+	elif ray_right.is_colliding():
+		direction_vector = Vector2(0, 1)
+	elif ray_left.is_colliding():
+		direction_vector = Vector2(0, -1)
+
+func _physics_process(delta) -> void:
+	move()
+	verify_direction()
 
 
-func _on_DetectionArea_body_exited(body):
-	if body.name == "Player":
-		pass
+func move() -> void:
+	velocity = direction_vector * speed
+	velocity = move_and_slide(velocity)
 
+
+func verify_direction() -> void:
+	if ray_up.is_colliding():
+		if ray_right.is_colliding():
+			direction_vector = Vector2(0, 1)
+		else:
+			direction_vector = Vector2(1, 0)
+	elif ray_down.is_colliding():
+		if ray_left.is_colliding():
+			direction_vector = Vector2(0, -1)
+		else:
+			direction_vector = Vector2(-1, 0)
+	elif ray_right.is_colliding():
+		if ray_down.is_colliding():
+			direction_vector = Vector2(-1, 0)
+		else:
+			direction_vector = Vector2(0, 1)
+	elif ray_left.is_colliding():
+		if ray_up.is_colliding():
+			direction_vector = Vector2(1, 0)
+		else:
+			direction_vector = Vector2(0, -1)
