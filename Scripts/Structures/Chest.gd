89bc6702fcd10d1962f7c6b19d3f6365576ae31c
FastShowDev <zlFastShow@hotmail.com>
2022-11-15 01:29:44 -0300

    Adiciona função de drop mais genérica, funcional para cada sala que tenha um baú e quantidade de itens e tipos arbitrários

--- Scripts/Structures/Chest.gd
+++ Scripts/Structures/Chest.gd
@@ -1,5 +1,11 @@
 extends AnimatedSprite
 
+var drop_path: Array = ["res://Models/Itens/Heart.tscn", 
+						"res://Models/Itens/Bomb.tscn",
+						"res://Models/Itens/Key.tscn",
+						"res://Models/Itens/Coin.tscn"
+						 ]
+var drop_list: Array #[heart, bomb, key, coin]
 var object = preload("res://Models/Itens/Bomb.tscn")
 var object2 = preload("res://Models/Itens/Key.tscn")
 var object_scene: PackedScene
@@ -23,34 +29,39 @@ func _input(event: InputEvent) -> void:
 		drop_object()
 
 func drop_object() -> void:
-	#var object = drop.instance()
-	var drop1 = object.instance()
-	var drop2 = object2.instance()
-	#var object = object_scene.instance()
-	#owner.get_node("Bombs").add_child(object)
-	owner.get_node("Drops").add_child(drop1)
-	owner.get_node("Drops").add_child(drop2)
-	play_drop_animation(drop1, position, 10)
-	play_drop_animation(drop2, position, 50)	
+	var i: int  = 0
+	for ammount in drop_list:
+		while ammount > 0:
+			var drop: PackedScene = load(drop_path[i])
+			var object = drop.instance()
+			owner.get_node("Drops").add_child(object)
+			var pos = randi() % 200 - 100
+			play_drop_animation(object, position, pos)
+			ammount -= 1
+		i += 1
+#	#var object = drop.instance()
+#	var drop1 = object.instance()
+#	var drop2 = object2.instance()
+#	#var object = object_scene.instance()
+#	#owner.get_node("Bombs").add_child(object)
+#	owner.get_node("Drops").add_child(drop1)
+#	owner.get_node("Drops").add_child(drop2)
+#	play_drop_animation(drop1, position, 10)
+#	play_drop_animation(drop2, position, 50)	
+
 
 func play_drop_animation(drop, position: Vector2, offset: float) -> void:
-	tween.interpolate_property(drop, "position", position + Vector2(20,0), position + Vector2(20, offset), 0.3, Tween.TRANS_QUAD,
+	tween.interpolate_property(drop, "position", position + Vector2(20,0), position + Vector2(offset, 0), 0.3, Tween.TRANS_QUAD,
 								Tween.EASE_OUT)
 	tween.start()
 	
-	#yield(tween, "tween_completed")
-	
-	#tween.interpolate_property(drop, "position", position + Vector2(20,0), position + Vector2(20, offset), 0.3, Tween.TRANS_SINE,
-								#Tween.EASE_IN)
-	#tween.start()
-	
 	
 func _on_Area2D_body_entered(_player: KinematicBody2D) -> void:
-	if _player is KinematicBody2D and not is_open:
+	if _player.name == "Player" and not is_open:
 		is_player_inside = true
 		interact_baloom.show()
 
 func _on_Area2D_body_exited(_player: KinematicBody2D) -> void:
-	if _player is KinematicBody2D and not is_open:
+	if _player.name == "Player" and not is_open:
 		is_player_inside = false
 		interact_baloom.hide()
